4db35dc1b0a6352f3af89ebcb2dde3d65609ae4a
NorthernBlow <evolution.is.not.theory@gmail.com>
2022-03-10 19:17:29 +0300

    so it begins

---
+++ catalog/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+
+class RegisterForm(forms.Form):
+    username = forms.CharField(label=u"Имя пользователя")
+    first_name = forms.CharField(label=u"Имя")
+    last_name = forms.CharField(label=u"Фамилия")
+    email = forms.EmailField(label=u"Email", required=False)
+    password = forms.CharField(label=u"Пароль", widget=forms.PasswordInput)
+    password_confirm = forms.CharField(label=u"Подтвердите пароль", widget=forms.PasswordInput)
+
+    def is_valid_or_not(self):
+        valid = super(RegisterForm, self).is_valid()   # may be to do
+        if self.cleaned_data['password'] != self.cleaned_data['password_confirm']:
+            self.add_error("password_confirm", u"Пароли не совпадают")
+            return False
+        return valid
