080c42768b6d6573e406e1fe32f4f80809cab568
NorthernBlow <evolution.is.not.theory@gmail.com>
2022-03-10 20:57:57 +0300

    some fixes

--- catalog/models.py
+++ catalog/models.py
@@ -11,5 +11,5 @@ class Profile(models.Model):
     first_name = models.CharField(max_length=100, verbose_name=u"Имя")
     last_name = models.CharField(max_length=100, verbose_name=u"Фамилия")
     avatar = models.FileField(null=True, blank=True, verbose_name=u"Аватар")
-    email = models.CharField(max_length=100, verbose_name=u"E-mail")
+    e_mail = models.CharField(max_length=100, verbose_name=u"E-mail")
     gender = models.CharField(max_length=10, verbose_name=u"Пол", choices=GENDER_CHOICES, default="male")
